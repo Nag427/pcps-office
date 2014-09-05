8b60f3c568c1a71b983000a769af7c21cc57754e
Nag427 <nagdasari@ymail.com>
2014-09-05 10:02:55 +0530

    PCPS off
    asd

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div align="center">
+<form>
+User Name:
+<input type="text" id="username" placeholder="enter user name"/></br>
+Password:
+<input type="password" id="password" placeholder="password"/></br>
+<input type="submit"/>
+
+
+</form>
+
+
+
+</div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Registration Form</title>
+</head>
+
+<body>
+<div align="center">
+<form>
+<table>
+<tr><td>
+User Name:</td><td>
+<input type="text" id="username" placeholder="enter user name"/></br></td></tr><tr><td>
+Email Id:</td><td>
+<input type="email" id="email" placeholder="enter  valid email"/></br></td></tr><tr><td>
+Password:</td><td>
+<input type="password" id="password" placeholder="password"/></br></td></tr><tr><td>
+Confirm Password:</td><td>
+<input type="password" id="confirmPassword" placeholder="re-enter password"/></br></td></tr><tr><td>
+Age:</td><td>
+<input type="number" id="age" placeholder="enter your age"/></br></td></tr><tr><td>
+Phone Number:</td><td>
+<input type="number" id="phone" placeholder="enter your phone number"/></br></td></tr><tr><td colspan="2" ><div align="center">
+<input type="submit" value="Register"/></div></td></tr>
+
+</table>
+</form>
+
+
+
+</div>
+
+
+</body>
+</html>
